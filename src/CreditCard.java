69874f0006f68d3406ccee366662d89ce8453e72
Lufr23 <Luiza.reis023@gmail.com>
2024-02-02 23:17:16 -0300

    first commit

---
+++ src/CreditCard.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class CreditCard {
+    private double limitCard;
+    private double balance;
+    ArrayList<Sales> sales;
+
+    public void creditCard (double limitCard){
+        this.limitCard = limitCard;
+        this.balance = balance;
+        this.sales = new ArrayList<>();
+    }
+
+    public boolean releaseSale(Sales sales){
+        if(this.saldo >= getValor())
+    }
+
+    public double getLimitCard() {
+        return limitCard;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
+
