f863072ebe974a154037b19bbeacd3802bd98486
Lufr23 <Luiza.reis023@gmail.com>
2024-02-16 09:39:42 -0300

    refactoring the main class

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
         double limit = 2500.00;
         CreditCard card = new CreditCard(2500.00);
 
-        int sair =1;
-        while (sair != 0){
+        int sair = 1;
+        while (sair != 0) {
 
             System.out.println("Choose your option: \n");
             System.out.println("1. Pay for a purchase ");
@@ -16,23 +16,21 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             int choose = scanner.nextInt();
 
-            switch (choose){
-                case 1 :
+            switch (choose) {
+                case 1:
                     System.out.println("Describe your purchase:");
                     String product = scanner.next();
                     System.out.println("Enter the purchase price: ");
                     double price = scanner.nextDouble();
 
-                    Sales sale = new Sales(product,price);
+                    Sales sale = new Sales(product, price);
                     boolean releaseSales = card.releaseSale(sale);
 
-                    if (releaseSales){
-                        System.out.println("Credit card purchase of R$  " + sale.getValue() +" successful.");
-                       // System.out.println("Type 0 to exit and 1 to continue");
-                        //sair = scanner.nextInt();
-                    }else{
+                    if (releaseSales) {
+                        System.out.println("Credit card purchase of R$  " + sale.getValue() + " successful.");
+                    } else {
                         System.out.println("Insufficient balance ");
-                        sair =0;
+                        sair = 0;
                     }
                     break;
                 case 2:
@@ -41,12 +39,12 @@ public class Main {
                     Collections.sort(card.getSales());
 
                     System.out.println(card.getSales());
-                   for (Sales c : card.getSales()) {
-                        System.out.println(c.getProduct() + "-"+ "Total R$" + c.getValue());
-                   }
+                    for (Sales c : card.getSales()) {
+                        System.out.println(c.getProduct() + "-" + "Total R$" + c.getValue());
+                    }
                     System.out.println("*******************************\n");
 
-                    System.out.println("Invoice amount: " + (card.getLimitCard()- card.getBalance()));
+                    System.out.println("Invoice amount: " + (card.getLimitCard() - card.getBalance()));
                     System.out.println("Limit avaiable: " + card.getBalance());
                     break;
             }
